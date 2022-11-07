a22c447c2ed8ec3a6353e4a07e764f2c49007ce3
hmbadhon2 <hmmizanurrahmanbadhon@gmail.com>
2022-11-08 02:34:27 +0600

    header, footers added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router/Route/Route';
 
 function App() {
   return (
     <div className="App">
      
+     <RouterProvider router={router}></RouterProvider>
+
     </div>
   );
 }
